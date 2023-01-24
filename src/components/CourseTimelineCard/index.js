6e954540a1efb12a9ea04010bdf5e75dab6ec64a
rahul <rahulmahto0507@gmail.com>
2023-01-24 09:27:57 +0000

    first commit

---
+++ src/components/CourseTimelineCard/index.js
@@ -0,0 +1,30 @@
+import {AiFillClockCircle} from 'react-icons/ai'
+
+import './index.css'
+
+const CourseTimelineCard = props => {
+  const {courseDetails} = props
+  const {courseTitle, description, duration, tagsList} = courseDetails
+
+  return (
+    <>
+      <div className="card-container">
+        <h1 className="course-title">{courseTitle}</h1>
+        <div className="clock-container">
+          <AiFillClockCircle color="#171f46" />
+          <p className="duration">{duration}</p>
+        </div>
+      </div>
+      <p className="course-description">{description}</p>
+      <ul className="list-container">
+        {tagsList.map(eachTag => (
+          <li className="list-type" key={eachTag.id}>
+            <p className="course-tag">{eachTag.name}</p>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+export default CourseTimelineCard
